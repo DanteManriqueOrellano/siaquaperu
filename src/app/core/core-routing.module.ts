ed3e050155649c655b54fc0fa11ac7a98281a37d
DanteManriqueOrellano <relaxurbano@me.com>
2020-01-30 14:22:59 +0100

    update cache

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from '../main/main/main.component';
 import { ConfiguracionComponent } from '../configuracion/configuracion/configuracion.component';
+import { MenuizquierdoComponent } from './shell/menuizquierdo/menuizquierdo.component';
 
 const routes: Routes = [
   {path:'',component:MainComponent},
-  {path:'project/projectid/settings/general', component: ConfiguracionComponent}
+  {path:'menuizquierdo/project/projectid/settings/general', component: ConfiguracionComponent},
+  {path:'menuizquierdo', component:MenuizquierdoComponent}
 ];
 
 @NgModule({
